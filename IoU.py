054f30e6366af2a371f5508ef010c1670fe7b7f3
Md Mahmudul Alam <37298971+MahmudulAlam@users.noreply.github.com>
2018-08-25 17:42:58 +0600

    Add files via upload

---
+++ IoU.py
@@ -0,0 +1,14 @@
+def iou(boxA, boxB):
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    interArea = (xB - xA + 1) * (yB - yA + 1)
+    if interArea < 0:
+        interArea = 0
+
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return iou
